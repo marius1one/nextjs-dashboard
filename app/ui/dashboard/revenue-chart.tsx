e0e03bfeeca8fe6be86e43a5a5adf578014ab0e3
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2025-07-26 23:09:00 +0100

    colors

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -28,8 +28,8 @@ export default async function RevenueChart() {
       </h2>
 
       {
-        <div className="rounded-xl bg-gray-50 p-4">
-          <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+        <div className="rounded-xl bg-zinc-900 p-4">
+          <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-zinc-900 p-4 md:gap-4">
             <div
               className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
               style={{ height: `${chartHeight}px` }}
@@ -45,7 +45,7 @@ export default async function RevenueChart() {
                 className="flex flex-col items-center gap-2"
               >
                 <div
-                  className="w-full rounded-md bg-blue-300"
+                  className="w-full rounded-md bg-amber-100"
                   style={{
                     height: `${(chartHeight / topLabel) * month.revenue}px`,
                   }}
