20050c2e20a4e13fe135df765f3cb70626ffebbb
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2025-08-11 15:55:07 +0100

    sql

--- auth.ts
+++ auth.ts
@@ -2,6 +2,21 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
+import type { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
